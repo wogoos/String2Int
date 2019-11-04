302f2dfe5c64dc9dabbe6525452f659190218710
Oscar Goos <wogoos@users.noreply.github.com>
2019-11-04 02:01:59 -0600

    Update str2int_Dynamic.cpp

--- str2int_Dynamic.cpp
+++ str2int_Dynamic.cpp
@@ -4,15 +4,16 @@
 	Author		: Oscar Goos
 	Email		: wogoos@gmx.com
 	Code		: C++, Flash : , RAM
-	Contrcoller : x86, Atmega, esp8266
-	Last tested : 4 - Nov - 2019
+	Contrcoller 	: x86, Atmega, esp8266
+	Last tested 	: 4 - Nov - 2019
 
 	Notes		: Command list can have any sequenc of commands/str seperated by commas.
-				: Best practice place short str/cmds in the beginning of the string, string pattren may not repeat.
-				: Arduino programmers, the list string may be located in PROGMEM
-				: This is the dynamic str2int version a change of the list string has no effect on the values asociated with the string
-				: the return values can have any you like associated with the string or command
-				: It is tested for X86 but with small adaption can be used for the other platforms.
+			: Best practice place short str/cmds in the beginning of the string, string pattren may not repeat.
+			: Arduino programmers, the list string may be located in PROGMEM
+			: This is the dynamic str2int version a change of the list string has no effect on the values asociated with the string
+			: The return values can have any value you like associated with the string or command
+			: It is tested for X86 but with small adaption can be used for the other platforms.
+			: Initialisation of the str.pos list is automatic taken care of.
 */
 
 #include <stdio.h>
@@ -24,13 +25,13 @@
 struct str2enum {
 	bool		initflag	= false;
 	uint8_t		cnt;
-	uint8_t		pos[10]		= { 0, 1, 2, 3, 4, 5, 6};											// buffer to hold the string position of each string/command in the list string
-	uint8_t		rc[10]		= { 10, 20, 30, 40, 50,60, 255 };									// the retrun codes associated with the string/command
+	uint8_t		pos[10]		= { 0, 1, 2, 3, 4, 5, 6};						// buffer to hold the string position of each string/command in the list string
+	uint8_t		rc[10]		= { 10, 20, 30, 40, 50,60, 255 };					// the retrun codes associated with the string/command
 	char		list[60]	= "cmd_one,cmd_two,cmd_three,cmd_four,cmd_five,cmd_six,error";		// any sequence of character patterns needed to associate with a value.
-	char		buf[30]		= "longest command in the list";									// buffer to hold a command/string character pattern 
+	char		buf[30]		= "longest command in the list";					// buffer to hold a command/string character pattern 
 }str;
 
-void init_str2enum(){																			// Initialises the str.pos values for all charater patterns
+void init_str2enum(){												// Initialises the str.pos values for all charater patterns
 	uint8_t i = 1;
 	for (uint8_t n = 1; n <= strlen(str.list); n++) if (*(str.list + n) == ',') {				
 		printf("List index %d: %d\n", i, n+1);
